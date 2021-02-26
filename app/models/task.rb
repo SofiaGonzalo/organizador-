a78bfad355ac649ee06629e30ca5c41231695103
Sofia Gonzalez <sofiagonzaloc@outlook.com>
2021-02-25 18:12:30 -0700

    Parte 1 del documento, todo funciona

---
+++ app/models/task.rb
@@ -0,0 +1,13 @@
+class Task < ApplicationRecord
+  belongs_to :category
+
+  validates :name, :description, presence: true
+    validates :name, uniqyeness: {case_sensitive: false}
+    validates :due_date_validity
+
+    def due_date_validity 
+      return if due_date.blank?
+      return if due_date > Date.today
+      errors.add :due_date, I18n.t('task,errors,invalid_due_date')
+    end
+end
